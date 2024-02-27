d908e75249b3d44a340cb753b47988d7fa2c3383
lky <skyvip2633@gmail.com>
2024-02-27 22:33:20 +0900

    [style] 스타일 수정

--- app/page.tsx
+++ app/page.tsx
@@ -15,14 +15,14 @@ export default function Home() {
         <span className="my-4">logo</span>
       </div>
 
-      <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+      <div className="grid text-center lg:max-w-5xl lg:w-2/3 lg:mb-0 lg:grid-cols-3 lg:text-left">
         {cate.map((items) => {
           const { title, link, context } = items;
           return (
             <Link
               key={title}
               href={link}
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              className="rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             >
               <h2 className={`mb-3 text-2xl font-semibold`}>
                 {title}{" "}
