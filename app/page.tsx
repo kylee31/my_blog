4b50722bb4422a5f338c73127cdb88dcfe929a95
lky <skyvip2633@gmail.com>
2024-02-29 22:58:06 +0900

    [style] 블로그 스타일 수정

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,26 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-20 px-24">
       <div className="relative flex place-items-center">
-        <h1 className="mb-16 text-4xl font-extrabold text-black">{`231's Blog`}</h1>
+        <h1 className="mb-10 text-4xl font-extrabold text-black">{`231's Blog`}</h1>
       </div>
-
-      <div className="relative flex flex-col mb-16 place-items-center text-center">
+      <Image src="/main.gif" alt="" priority width={400} height={100} />
+      <div className="relative flex flex-col mt-5 mb-11 place-items-center text-center">
         <span>결과를 확인할 수 있는 활동을 좋아하는 개발자입니다.</span>
-        <span>가독성이 높은 코드, 단순하지만 깔끔한 디자인을 선호합니다.</span>
-        <div className="flex">
+        <span>
+          가독성이 높은 코드, 단순하지만 직관적인 디자인을 선호합니다.
+        </span>
+        <div className="flex mt-5">
           <a
-            className="mt-5 mr-3"
+            title="velog에서 231's blog로 이동 중..🏃‍♂️"
+            className="mr-3"
             href="https://velog.io/@kylee31/posts"
             target="_blank"
           >
             <Image src="/velog.png" alt="no img" width={25} height={25} />
           </a>
-          <a className="mt-5" href="https://github.com/kylee31" target="_blank">
+          <a href="https://github.com/kylee31" target="_blank">
             <Image src="/github.png" alt="no img" width={25} height={25} />
           </a>
         </div>
@@ -33,7 +36,7 @@ export default function Home() {
             <Link
               key={title}
               href={link}
-              className="rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              className="rounded-lg border border-transparent px-5 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             >
               <h2 className={`mb-3 text-2xl font-semibold`}>
                 {title}{" "}
--- data/posts.ts
+++ data/posts.ts
@@ -4,20 +4,24 @@ export const myProject:myProjectType[]=[
     {
     title:"My Blog",
     slug:"myblog",
-    desc:"공부 내용을 정리하는 기록장"
+    desc:"공부 내용을 정리하는 기록장",
+    link:"https://twothreeone-blog.vercel.app/"
+},{
+    title:"Travel Plan",
+    slug:"travelplan",
+    desc:"지도 API, 지역축제 목록 API를 사용한 여행 계획 웹 서비스",
+    link:"http://travel-plan.kro.kr"
 },
 {
     title:"Movie Diary",
     slug:"moviediary",
-    desc:"그림 일기 형식으로 작성하는 영화 감상문"
+    desc:"그림 일기 형식으로 작성하는 영화 감상문",
+    link:"https://kylee31.github.io/movie_diary"
 },
 {
     title:"Book Mark",
     slug:"bookmark",
-    desc:"카테고리 및 메모 내용을 저장하는 웹 페이지"
-},{
-    title:"Travel Plan",
-    slug:"travelplan",
-    desc:"지도 API, 지역축제 목록 API를 사용한 여행 계획 웹 서비스"
+    desc:"카테고리 및 메모 내용을 저장하는 웹 페이지",
+    link:"https://book-mark-db594.web.app/"
 }
 ]
