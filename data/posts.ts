7514b5dd8f6fbb960f8fef966d8c1a167185f2c2
lky <skyvip2633@gmail.com>
2024-03-15 20:48:02 +0900

    [blog] 3/15 posting

--- data/posts.ts
+++ data/posts.ts
@@ -13,15 +13,15 @@ export const myProject:myProjectType[]=[
     link:"http://to-travel-is-to-live.kro.kr"
 },
 {
-    title:"Movie Diary",
-    slug:"moviediary",
-    desc:"그림 일기 형식으로 작성하는 영화 감상문",
-    link:"https://kylee31.github.io/movie_diary"
-},
-{
     title:"Book Mark",
     slug:"bookmark",
     desc:"카테고리 및 메모 내용을 저장하는 웹 페이지",
     link:"https://book-mark-db594.web.app/"
+},
+{
+    title:"Movie Diary",
+    slug:"moviediary",
+    desc:"그림 일기 형식으로 작성하는 영화 감상문",
+    link:"https://kylee31.github.io/movie_diary"
 }
 ]
